1aa582affeb36635e2a4e033fefa63251f956c32
josepmg <josemello@id.uff.br>
2021-02-27 20:48:53 -0300

    Update pubspec.yaml and Mixpanel class.

--- lib/core/platform/mixpanel.dart
+++ lib/core/platform/mixpanel.dart
@@ -1,12 +1,17 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
+import 'dart:developer' as dev;
 
 import 'package:cardio_flutter/core/platform/settings.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mixpanel_analytics/mixpanel_analytics.dart';
+import 'package:package_info/package_info.dart';
 
-const MIXPANEL_TOKEN = '7c27b5a82c6705e36ef57be2c65901fd';
+const MIXPANEL_TOKEN_DEV = 'f06f25864e88616b1384f5c2cb52b0a7';
+const MIXPANEL_TOKEN_PROD = '62ac1951cffac01b8373b44d03561b6d';
 const DISTINCT_ID = "distinct_id";
 const PHONE_OS = "phoneOS";
 const LOCALE = "locale";
@@ -17,10 +22,10 @@ class Mixpanel {
   static MixpanelAnalytics _mixpanel;
   static StreamController<String> _user$ = StreamController<String>.broadcast();
 
-  static _init() {
+  static _init(String versionName) {
     // _user$ ??= StreamController<String>.broadcast();
     _mixpanel ??= MixpanelAnalytics(
-      token: MIXPANEL_TOKEN,
+      token: kReleaseMode ? MIXPANEL_TOKEN_PROD : MIXPANEL_TOKEN_DEV,
       userId$: _user$.stream,
       verbose: true,
       shouldAnonymize: false,
@@ -31,8 +36,10 @@ class Mixpanel {
     );
   }
 
-  static Future<MixpanelResult> trackEvent(MixpanelEvents event, {String userId, Map<String, dynamic> data}) async {
-    _init();
+  static Future<MixpanelResult> trackEvent(MixpanelEvents event,
+      {String userId, Map<String, dynamic> data}) async {
+    String version = (await PackageInfo.fromPlatform()).version;
+    _init(version);
     if (userId == null) {
       try {
         userId = GetIt.instance<Settings>().getUserId();
@@ -46,13 +53,29 @@ class Mixpanel {
     data[DISTINCT_ID] = userId;
     data[PHONE_OS] = Platform.isAndroid ? "android" : "iOS";
     data[LOCALE] = Platform.localeName;
+    var date = DateTime.now().toUtc();
+    print(date.toString());
+    data['timeStamp'] = date.toString();
 
-    var result = await _mixpanel.track(event: _getEventString(event), properties: data);
-    return result ? MixpanelResult(isSuccessful: true) : MixpanelResult(errorText: "Erro no Mixpanel");
+    var result = await _mixpanel.track(
+      event: _getEventString(event),
+      properties: data,
+      time: date,
+    );
+    result
+        ? dev.log(
+            "Sucesso ao enviar evento ${_getEventString(event)} - ${data.toString()}!",
+            name: "Mixpanel - trackEvent",
+          )
+        : dev.log(
+            "Falha ao enviar evento ${_getEventString(event)} - ${data.toString()} :(",
+            name: "Mixpanel - trackEvent",
+          );
   }
 
   static updateProfile(String userId, Map<String, dynamic> data) async {
-    _init();
+    String version = (await PackageInfo.fromPlatform()).version;
+    _init(version);
     _user$?.add(userId ?? 'Usuário não identificado');
     if (data == null) data = Map<String, dynamic>();
     data[DISTINCT_ID] = userId;
@@ -61,7 +84,23 @@ class Mixpanel {
       value: data,
       time: DateTime.now().toUtc(),
     );
-    return result ? MixpanelResult(isSuccessful: true) : MixpanelResult(errorText: "Erro no Mixpanel");
+    result
+        ? dev.log(
+            "Sucesso ao enviar dados - ${data.toString()}!",
+            name: "Mixpanel - updateProfile",
+          )
+        : dev.log(
+            "Falha ao enviar dados - ${data.toString()} :(",
+            name: "Mixpanel - updateProfile",
+          );
+  }
+
+  static trackOnSelectNotification(String payload) async {
+    if (payload != null && payload.isNotEmpty) {
+      // Decodes JSON
+      Map<String, dynamic> map = json.decode(payload);
+      trackEvent(MixpanelEvents.OPEN_NOTIFICATION, data: map);
+    }
   }
 }
 
@@ -74,29 +113,33 @@ class MixpanelResult {
 
 enum MixpanelEvents {
   // General actions
-  OPEN_APP, // feito
-  DO_LOGIN, // feito
-  DO_LOGOUT, // feito
-  READ_ORIENTATIONS, // feito
-  READ_INFORMATION, // feito
-  READ_QUESTIONS, // feito
-  OPEN_HISTORY, // feito
+  OPEN_APP,
+  OPEN_PAGE,
+  DO_LOGIN,
+  DO_LOGOUT,
+  OPEN_HISTORY,
+  OPEN_NOTIFICATION,
+  OPEN_RECOMENDATION_DETAIL,
+  OPEN_ACTION_DETAIL,
+  OPEN_ORIENTATION_ITEM,
+  OPEN_INFO_ITEM,
+  OPEN_HELP_ITEM,
 
   // Professional actions
-  REGISTER_PROFESSIONAL, // feito
-  REGISTER_PATIENT, // feito
-  EDIT_PROFESSIONAL, // feito
-  EDIT_PATIENT, // feito
-  DELETE_PATIENT, // feito
-  OPEN_PATIENT, //feito
-  RECOMMEND_ACTION, //feito
-  EDIT_RECOMMENDATION, // feito
-  DELETE_RECOMMENDATION, // feito
+  REGISTER_PROFESSIONAL,
+  REGISTER_PATIENT,
+  EDIT_PROFESSIONAL,
+  EDIT_PATIENT,
+  DELETE_PATIENT,
+  OPEN_PATIENT,
+  RECOMMEND_ACTION,
+  EDIT_RECOMMENDATION,
+  DELETE_RECOMMENDATION,
 
   // Patient actions
-  DO_ACTION, // feito
-  EDIT_ACTION, // feito
-  DELETE_ACTION, // feito
+  DO_ACTION,
+  EDIT_ACTION,
+  DELETE_ACTION,
 }
 
 String _getEventString(MixpanelEvents event) {
@@ -107,12 +150,6 @@ String _getEventString(MixpanelEvents event) {
       return "DO_LOGIN";
     case MixpanelEvents.DO_LOGOUT:
       return "DO_LOGOUT";
-    case MixpanelEvents.READ_ORIENTATIONS:
-      return "READ_ORIENTATIONS";
-    case MixpanelEvents.READ_INFORMATION:
-      return "READ_INFORMATION";
-    case MixpanelEvents.READ_QUESTIONS:
-      return "READ_QUESTIONS";
     case MixpanelEvents.OPEN_HISTORY:
       return "OPEN_HISTORY";
     case MixpanelEvents.REGISTER_PROFESSIONAL:
@@ -139,6 +176,20 @@ String _getEventString(MixpanelEvents event) {
       return "EDIT_ACTION";
     case MixpanelEvents.DELETE_ACTION:
       return "DELETE_ACTION";
+    case MixpanelEvents.OPEN_PAGE:
+      return "OPEN_PAGE";
+    case MixpanelEvents.OPEN_NOTIFICATION:
+      return "OPEN_NOTIFICATION";
+    case MixpanelEvents.OPEN_RECOMENDATION_DETAIL:
+      return "OPEN_RECOMENDATION_DETAIL";
+    case MixpanelEvents.OPEN_ACTION_DETAIL:
+      return "OPEN_ACTION_DETAIL";
+    case MixpanelEvents.OPEN_ORIENTATION_ITEM:
+      return "OPEN_ORIENTATION_ITEM";
+    case MixpanelEvents.OPEN_HELP_ITEM:
+      return "OPEN_HELP_ITEM";
+    case MixpanelEvents.OPEN_INFO_ITEM:
+      return "OPEN_INFO_ITEM";
     default:
       return "UNKNOWN_EVENT";
   }
