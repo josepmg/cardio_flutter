94c00805bf437001e32a20fb79bbb62f47c8f012
josepmg <josemello@id.uff.br>
2021-02-28 22:15:42 -0300

    Refactor help and orientation features.

--- lib/features/help/presentation/pages/patient_help_page.dart
+++ lib/features/help/presentation/pages/patient_help_page.dart
@@ -1,39 +1,69 @@
+import 'package:cardio_flutter/core/platform/mixpanel.dart';
 import 'package:cardio_flutter/features/auth/presentation/pages/basePage.dart';
 import 'package:cardio_flutter/features/orientations/widgets/clickable_item.dart';
+import 'package:cardio_flutter/resources/dimensions.dart';
 import 'package:cardio_flutter/resources/strings.dart';
 import 'package:flutter/material.dart';
+import 'package:focus_detector/focus_detector.dart';
 
 class PatientHelpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BasePage(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              ClickableItem(
-                  title: Strings.help1,
-                  text: TextSpan(text: Strings.help_answer1)),
-              ClickableItem(
-                  title: Strings.help2,
-                  text: TextSpan(text: Strings.help_answer2)),
-              ClickableItem(
-                  title: Strings.help3,
-                  text: TextSpan(text: Strings.help_answer3)),
-              ClickableItem(
-                  title: Strings.help4,
-                  text: TextSpan(text: Strings.help_answer4)),
-              ClickableItem(
-                  title: Strings.help5,
-                  text: TextSpan(text: Strings.help_answer5)),
-              ClickableItem(
-                  title: Strings.help6,
-                  text: TextSpan(text: Strings.help_answer6)),
-              ClickableItem(
-                  title: Strings.help7,
-                  text: TextSpan(text: Strings.help_answer7)),
-            ],
+    return FocusDetector(
+      key: UniqueKey(),
+      onFocusGained: () {
+        Mixpanel.trackEvent(
+          MixpanelEvents.OPEN_PAGE,
+          data: {"pageTittle": "PatientHelpPage"},
+        );
+      },
+      child: BasePage(
+        recomendation: Strings.help,
+        body: SingleChildScrollView(
+          child: Container(
+            margin: Dimensions.getEdgeInsets(context, left: 15, top: 30),
+            width: double.infinity,
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  ClickableItem(
+                    title: Strings.help1,
+                    text: TextSpan(text: Strings.help_answer1),
+                    event: MixpanelEvents.OPEN_HELP_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.help2,
+                    text: TextSpan(text: Strings.help_answer2),
+                    event: MixpanelEvents.OPEN_HELP_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.help3,
+                    text: TextSpan(text: Strings.help_answer3),
+                    event: MixpanelEvents.OPEN_HELP_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.help4,
+                    text: TextSpan(text: Strings.help_answer4),
+                    event: MixpanelEvents.OPEN_HELP_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.help5,
+                    text: TextSpan(text: Strings.help_answer5),
+                    event: MixpanelEvents.OPEN_HELP_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.help6,
+                    text: TextSpan(text: Strings.help_answer6),
+                    event: MixpanelEvents.OPEN_HELP_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.help7,
+                    text: TextSpan(text: Strings.help_answer7),
+                    event: MixpanelEvents.OPEN_HELP_ITEM,
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- lib/features/orientations/presentation/pages/orientations_page.dart
+++ lib/features/orientations/presentation/pages/orientations_page.dart
@@ -1,114 +1,152 @@
+import 'package:cardio_flutter/core/platform/mixpanel.dart';
 import 'package:cardio_flutter/features/auth/presentation/pages/basePage.dart';
 import 'package:cardio_flutter/features/orientations/widgets/clickable_item.dart';
+import 'package:cardio_flutter/resources/cardio_colors.dart';
+import 'package:cardio_flutter/resources/dimensions.dart';
 import 'package:cardio_flutter/resources/strings.dart';
 import 'package:flutter/material.dart';
+import 'package:focus_detector/focus_detector.dart';
 
 class OrientationsPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return BasePage(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              ClickableItem(
-                title: Strings.orientation1,
-                text: TextSpan(text: Strings.orientation_answer1),
-              ),
-              ClickableItem(
-                title: Strings.orientation2,
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                        text: "Importante: ",
-                        style: TextStyle(
-                            color: Colors.red, fontWeight: FontWeight.bold)),
-                    TextSpan(text: Strings.orientation_answer2),
-                  ],
-                 
-                ),
-              ),
-              ClickableItem(
-                title: Strings.orientation3,
-                text: TextSpan(
-                  children: [
-                    TextSpan(text: Strings.orientation_answer3_1),
-                    TextSpan(
-                        text: " EVITE ",
-                        style: TextStyle(
-                            color: Colors.red, fontWeight: FontWeight.bold)),
-                    TextSpan(text: Strings.orientation_answer3_2),
-                    TextSpan(
-                      text: "Prefira temperos naturais:  ",
-                      style: TextStyle(
-                          color: Colors.black,
-                          decoration: TextDecoration.underline,
-                          fontWeight: FontWeight.bold),
+    return FocusDetector(
+      key: UniqueKey(),
+      onFocusGained: () {
+        Mixpanel.trackEvent(
+          MixpanelEvents.OPEN_PAGE,
+          data: {"pageTittle": "OrientationsPage"},
+        );
+      },
+      child: BasePage(
+        recomendation: Strings.orientations,
+        body: SingleChildScrollView(
+          child: Container(
+            margin: Dimensions.getEdgeInsets(context, left: 15, top: 30),
+            width: double.infinity,
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  ClickableItem(
+                    title: Strings.orientation1,
+                    text: TextSpan(text: Strings.orientation_answer1),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation2,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "Importante:",
+                          style: TextStyle(
+                            color: CardioColors.black,
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        TextSpan(text: Strings.orientation_answer2),
+                      ],
                     ),
-                    TextSpan(text: Strings.orientation_answer3_3),
-                  ],
-                 
-                  
-                ),
-              ),
-              ClickableItem(
-                title: Strings.orientation4,
-                text: TextSpan(text: Strings.orientation_answer4),
-              ),
-              ClickableItem(
-                title: Strings.orientation5,
-                text: TextSpan(text: Strings.orientation_answer5),
-              ),
-              ClickableItem(
-                title: Strings.orientation6,
-                text: TextSpan(text: Strings.orientation_answer6),
-              ),
-              ClickableItem(
-                title: Strings.orientation7,
-                text: TextSpan(text: Strings.orientation_answer7),
-              ),
-              ClickableItem(
-                title: Strings.orientation8,
-                text: TextSpan(text: Strings.orientation_answer8),
-              ),
-              ClickableItem(
-                title: Strings.orientation9,
-                text: TextSpan(text: Strings.orientation_answer9),
-              ),
-              ClickableItem(
-                title: Strings.orientation10,
-                text: TextSpan(text: Strings.orientation_answer10),
-              ),
-              ClickableItem(
-                title: Strings.orientation11,
-                text: TextSpan(text: Strings.orientation_answer11),
-              ),
-              ClickableItem(
-                title: Strings.orientation12,
-                text: TextSpan(
-                  children: [
-                    TextSpan(text: Strings.orientation_answer12_1),
-                    TextSpan(
-                      text: " Para diminuir a ansiedade: ",
-                      style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          fontWeight: FontWeight.bold),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation3,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(text: Strings.orientation_answer3_1),
+                        TextSpan(
+                          text: "Evite",
+                          style: TextStyle(
+                            color: CardioColors.black,
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        TextSpan(text: Strings.orientation_answer3_2),
+                        TextSpan(
+                          text: "Prefira temperos naturais:  ",
+                          style: TextStyle(
+                            color: CardioColors.black,
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        TextSpan(text: Strings.orientation_answer3_3),
+                      ],
                     ),
-                    TextSpan(text: Strings.orientation_answer12_2),
-                  ],
-                  
-                ),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation4,
+                    text: TextSpan(text: Strings.orientation_answer4),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation5,
+                    text: TextSpan(text: Strings.orientation_answer5),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation6,
+                    text: TextSpan(text: Strings.orientation_answer6),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation7,
+                    text: TextSpan(text: Strings.orientation_answer7),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation8,
+                    text: TextSpan(text: Strings.orientation_answer8),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation9,
+                    text: TextSpan(text: Strings.orientation_answer9),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation10,
+                    text: TextSpan(text: Strings.orientation_answer10),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation11,
+                    text: TextSpan(text: Strings.orientation_answer11),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation12,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(text: Strings.orientation_answer12_1),
+                        TextSpan(
+                          text: " Para diminuir a ansiedade: ",
+                          style: TextStyle(
+                            color: CardioColors.black,
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        TextSpan(text: Strings.orientation_answer12_2),
+                      ],
+                    ),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation13,
+                    text: TextSpan(text: Strings.orientation_answer13),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                  ClickableItem(
+                    title: Strings.orientation14,
+                    text: TextSpan(text: Strings.orientation_answer14),
+                    event: MixpanelEvents.OPEN_ORIENTATION_ITEM,
+                  ),
+                ],
               ),
-              ClickableItem(
-                  title: Strings.orientation13,
-                  text: TextSpan(text: Strings.orientation_answer13)),
-              ClickableItem(
-                title: Strings.orientation14,
-                text: TextSpan(text: Strings.orientation_answer14),
-              ),
-            ],
+            ),
           ),
         ),
       ),
